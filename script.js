81acc636a01c2f3ec60f267c895192c2cf05a5cc
Mauricio Spesot <36648340+mauriciospesot@users.noreply.github.com>
2018-05-06 15:18:55 -0300

    Update script.js
    Change variable name css to currentCss
    Added leftArrow and rightArrow variables
    Set default body background
    Set default h3 text content
    Delete setKeyRandomGradient() function
    Added setPreviousGradient() function
    Delete body and container addEventListener()
    Added rightArrow and leftArrow addEventListener()

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var css = document.querySelector("h3");
+var CurrentCss = document.querySelector("h3");
 var inputColor1 = document.querySelector(".color1");
 var inputColor2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-var container = document.querySelector(".container");
+var leftArrow = document.querySelector(".left");
+var rightArrow = document.querySelector(".right");
 var previousColors = [["#02AAB0", "#00CDAC"]];
 var lastElementArray = 0;
+body.style.background = "linear-gradient(to right, #02AAB0, #00CDAC)";
+CurrentCss.textContent = body.style.backgroundImage + ";";
 
 
 function getRandomColor() {
@@ -20,7 +23,7 @@ function getRandomColor() {
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + inputColor1.value + ", " + inputColor2.value + ")";
-    css.textContent = body.style.backgroundImage + ";";
+    CurrentCss.textContent = body.style.backgroundImage + ";";
 }
 
 function setRandomGradient() {
@@ -28,23 +31,21 @@ function setRandomGradient() {
     var randomColor2 = getRandomColor();
 
     previousColors.push([randomColor1,randomColor2]);
-    
+
     lastElementArray = previousColors.length - 1;
     body.style.background = "linear-gradient(to right, " + randomColor1 + ", " + randomColor2 + ")";
     inputColor1.value = randomColor1;
     inputColor2.value = randomColor2;
-    css.textContent = body.style.backgroundImage + ";";
+    CurrentCss.textContent = body.style.backgroundImage + ";";
 }
 
-function setKeyRandomGradient(event) {
-    if (event.keyCode === 13) {
-        setRandomGradient();
-    } else if (event.keyCode === 8 && lastElementArray > 0) {
+function setPreviousGradient() {
+    if (lastElementArray > 0) {
         lastElementArray = lastElementArray - 1;
         body.style.background = "linear-gradient(to right, " + previousColors[lastElementArray][0] + ", " + previousColors[lastElementArray][1] + ")";
         inputColor1.value = previousColors[lastElementArray][0];
         inputColor2.value = previousColors[lastElementArray][1];
-        css.textContent = body.style.backgroundImage + ";";
+        CurrentCss.textContent = body.style.backgroundImage + ";";
     }
 }
 
@@ -52,6 +53,6 @@ inputColor1.addEventListener("input", setGradient);
 
 inputColor2.addEventListener("input", setGradient);
 
-container.addEventListener("click", setRandomGradient);
+rightArrow.addEventListener("click", setRandomGradient);
 
-body.addEventListener("keydown", setKeyRandomGradient);
+leftArrow.addEventListener("click", setPreviousGradient);
